6f8aa090ff2d337a30a969d9a272db734df8d6f0
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2020-06-25 11:03:12 -0300

    Initial Commit

---
+++ PokedexMVP/PokedexMVP.h
@@ -0,0 +1,19 @@
+//
+//  PokedexMVP.h
+//  PokedexMVP
+//
+//  Created by Lucas Daniel on 25/06/20.
+//  Copyright © 2020 Lucas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexMVP.
+FOUNDATION_EXPORT double PokedexMVPVersionNumber;
+
+//! Project version string for PokedexMVP.
+FOUNDATION_EXPORT const unsigned char PokedexMVPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexMVP/PublicHeader.h>
+
+
